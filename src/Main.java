ab9dff4761a0462a6aa901bd8c5e536ccf45b976
Alexandre Torres <alex_048@msn.com>
2022-10-24 18:45:59 +0100

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import gui.GameGuiMain;
+
+/**
+ * Created by Alexandre Torres on IntelliJ IDEA.
+ * Date: 24/10/2022
+ * Time: 18:43
+ * <p>
+ * Details of the file:
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        GameGuiMain gui = new GameGuiMain();
+
+        gui.init();
+    }
+}
